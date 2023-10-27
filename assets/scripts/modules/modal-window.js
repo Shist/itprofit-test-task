3507d5c12cdd131b7fc9342a6e2e0a32942d8834
Shist <shist.pupust@mail.ru>
2023-10-27 06:33:26 +0300

    Now opening modal is preventing scrolling and adds some margin

--- assets/scripts/modules/modal-window.js
+++ assets/scripts/modules/modal-window.js
@@ -6,22 +6,45 @@ function modalWindow() {
   const closeBtn = document.querySelector(".modal-window-wrapper__close-btn");
   const okBtn = document.querySelector(".modal-window-wrapper__btn-ok");
 
+  function calcScroll() {
+    const div = document.createElement("div");
+
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.overflowY = "scroll";
+    div.style.visibility = "hidden";
+
+    document.body.appendChild(div);
+    const scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+  }
+
   btnOpenModal.addEventListener("click", () => {
     modalWindowWrapper.classList.remove("hidden-element");
+    document.body.classList.add("modal-open");
+    document.body.style.marginRight = `${calcScroll()}px`;
   });
+
+  function closeModal() {
+    modalWindowWrapper.classList.add("hidden-element");
+    document.body.classList.remove("modal-open");
+    document.body.style.marginRight = "0px";
+  }
 
   modalWindowWrapper.addEventListener("click", (e) => {
     if (e.target.classList.contains("modal-window-wrapper")) {
-      modalWindowWrapper.classList.add("hidden-element");
+      closeModal();
     }
   });
 
   closeBtn.addEventListener("click", () => {
-    modalWindowWrapper.classList.add("hidden-element");
+    closeModal();
   });
 
   okBtn.addEventListener("click", () => {
-    modalWindowWrapper.classList.add("hidden-element");
+    closeModal();
   });
 }
 
